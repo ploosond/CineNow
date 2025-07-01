991d9323c83154e1aca1d0a8463ca1087d001d57
Prajwol Devkota <prajwol.devkota016@gmail.com>
2025-07-01 17:12:53 +0200

    added clerk and inngest

--- server/app.js
+++ server/app.js
@@ -1,12 +1,17 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./src/inngest/index.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(clerkMiddleware());
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/ping", (request, response) => {
   response.json({ message: "PONG!" });
