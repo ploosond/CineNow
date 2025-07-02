6b5db7c30babf380ea1649bd774076413afaaf7e
Prajwol Devkota <prajwol.devkota016@gmail.com>
2025-07-02 15:18:08 +0200

    fixed User model

--- server/api/inngest/index.js
+++ server/api/inngest/index.js
@@ -1,5 +1,5 @@
 import { Inngest } from "inngest";
-import User from "../models/user.model";
+import User from "../models/user.model.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
